7cbbd82e62c3d8f2e838f08a8f547b37399f48e8
Carlos-SSH <carlos.anezeh@gmail.com>
2020-11-01 19:52:23 -0600

    Fixed issues in DB.

---
+++ GitHub/backend-js/src/app/routes/users.js
@@ -0,0 +1,34 @@
+const dbConnection = require('../../config/dbConnection');
+
+module.exports = app => {
+  
+  const connection = dbConnection();
+
+  app.get('/', (req, res) => {
+    connection.query('SELECT * FROM users', (err, result) => {
+      console.log(result);
+      res.render('users/users', {
+        users: result
+      });
+    });
+  });
+
+  app.post('/users', (req, res) => {
+    console.log(req.body);
+    const {name_user, email, kms} = req.body;
+    
+    
+    connection.query('INSERT INTO users SET?', {
+      name_user,
+      email,
+      kms
+    }, (err, result) => {
+      res.redirect('/');
+      if(parseFloat(req.body.kms >= 4)) {
+        console.log('Se añadió correctamente!');
+      } else {
+        console.log('Necesitas correr mas');
+      }
+    });
+  });
+}
